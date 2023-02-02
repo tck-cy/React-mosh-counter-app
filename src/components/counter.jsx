2e0e70525d122d1ea6b99075d7eeea04575d4666
tck-cy <99850439+tck-cy@users.noreply.github.com>
2023-02-01 20:23:44 -0800

    Completed react-mosh

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps);
+    console.log('prevState', prevState);
+    if (prevProps.counter.value != this.props.counter.value) {
+      //Ajax for copy sitting classes
+    }
+  };
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
+  
   render() {
     console.log('Counter - Rendered');
     return (
